56be7540946ba5c24c1dd8f30e8bc8b67c1151ce
Aysu VARGELOĞLU TR <61291903+aysuvargel@users.noreply.github.com>
2021-01-06 00:33:23 +0300

    Add files via upload

---
+++ drag drop/Sürükle Bırak/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Sürükle_Bırak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "Ayşe",
+            "Aysun",
+            "Nazmi",
+            "Ozan",
+            "Berkin",
+            "Ahmet",
+            "Bilgehan",
+            "Oğuz"});
+            this.listBox1.Location = new System.Drawing.Point(22, 17);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(128, 202);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(171, 17);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(128, 202);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox2_DragDrop);
+            this.listBox2.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox2_DragOver);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 239);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ drag drop/Sürükle Bırak/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sürükle_Bırak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int indeks;
+        private void listBox1_MouseDown(object sender, MouseEventArgs buton)
+        {
+            //farenin listbox içerisinde kliklenen yerin koordinatlrını alır.
+            Point nokta = new Point(buton.X, buton.Y);
+            //IndexFromPoint()=Listbox'ın kliklenen elemanının indexini verir.
+            indeks = listBox1.IndexFromPoint(nokta);
+            if (indeks==-1)
+            {
+                return;
+            }
+            if (buton.Button==MouseButtons.Left)
+            {
+                //sürüklenecek nesne alınıyor
+                listBox1.DoDragDrop(listBox1.Items[indeks], DragDropEffects.Move);
+            }
+
+        }
+
+        private void listBox2_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.KeyState==1)//1=sol tuş, 2=sağ tuş
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void listBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            int index = listBox1.SelectedIndices.Count - 1;
+            //for (int i = listBox1.SelectedIndices.Count - 1; i >=0; i--)
+            //{
+            //    listBox2.Items.Add(listBox1.SelectedItems[i]);
+            //    listBox1.Items.RemoveAt(listBox1.SelectedIndices[i]);
+            //}
+
+            while (index >=0)
+            {
+                listBox2.Items.Add(listBox1.SelectedItems[index]);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndices[index]);
+                index--;
+            }
+        }
+    }
+}
+
+//Üzerine bırakılacak nesne (listbox2) AllowDrop özelliğ true yapılmak zorundadır. 
